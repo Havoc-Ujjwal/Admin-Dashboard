3d2271902eab47647373cf731ea5b19fd6c7c9cd
Ujjwal Kumar <er.ujjwalkumar18@gmail.com>
2024-07-03 13:44:41 +0530

    Final Commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDom from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css'
+import App from './App'
+import { ContextProvider } from "./contexts/ContextProvider";
+
+ReactDom.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById('root'));
